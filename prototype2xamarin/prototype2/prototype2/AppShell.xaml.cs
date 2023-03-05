67f94b70ed4f2455ec5e45d32b22aaff1995e42c
KP125689 <P.RWEHUMBIZA-2021@hull.ac.uk>
2023-03-05 16:03:34 +0000

    1

---
+++ prototype2xamarin/prototype2/prototype2/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using prototype2.ViewModels;
+using prototype2.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace prototype2
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ prototype2xamarin/prototype2/prototype2/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using prototype2.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace prototype2.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
